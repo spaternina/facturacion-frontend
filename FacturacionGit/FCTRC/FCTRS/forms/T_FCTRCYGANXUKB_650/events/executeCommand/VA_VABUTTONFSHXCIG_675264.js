af438fa3b3e6d25c741fdd52a260cb6c01fc05cc
Willian Camacho <61927270+Will2020-cpu@users.noreply.github.com>
2021-10-17 23:52:09 -0500

    crear nueva rama

---
+++ FacturacionGit/FCTRC/FCTRS/forms/T_FCTRCYGANXUKB_650/events/executeCommand/VA_VABUTTONFSHXCIG_675264.js
@@ -0,0 +1,11 @@
+
+
+//Entity: Factura
+//Factura. (Button) View: AgregarFacturaForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONFSHXCIG_675264 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.Factura = true;
+
+};
